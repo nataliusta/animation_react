8b5d0086640e562f0ea3b5c359973d8e259d5b80
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-22 17:12:41 +0300

    Add animation for form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
       <header id="main-header">
         <h1>Your Challenges</h1>
         <motion.button 
-          whileHover={{ scale: 1.1 }}
+          whileHover={{ scale: 1.1, backgroundColor: '#8b11f0' }}
           transition={{ type: 'spring', stiffness: 500 }}
           onClick={handleStartAddNewChallenge} 
           className="button">
